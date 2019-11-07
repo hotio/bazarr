f2e8a0116210f54a2bce1114616db091d2f3518b
hotio <hotio@protonmail.com>
2019-11-07 11:31:05 +0100

    add github action

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###################
+version=$(curl -fsSL "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq -r .tag_name | sed s/v//g)
+app=BAZARR
+###################
+
+location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${location}" || exit 1
+
+find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+
+if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
+    git add ./*.Dockerfile
+    git commit -m "Updated version to: ${version}"
+    echo "Updated version to: ${version}"
+else
+    echo "Version is still: ${version}"
+fi
