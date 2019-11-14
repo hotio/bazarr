1ff24743dce1d4c4574c577866290b54dc28e8e0
hotio <hotio@protonmail.com>
2019-11-14 12:57:54 +0100

    update

--- update.sh
+++ update.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-###################
 version=$(curl -fsSL "https://api.github.com/repos/morpheus65535/bazarr/commits/development" | jq -r .sha)
-app=BAZARR
-###################
-
-location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "${location}" || exit 1
-
-find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG BAZARR_VERSION=.*$/ARG BAZARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
-
-if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
-    git add ./*.Dockerfile
-    git add ./.drone.yml
-    git commit -m "Updated version to: ${version}"
-    echo "Updated version to: ${version}"
-else
-    echo "Version is still: ${version}"
-fi
+echo "##[set-output name=version;]${version}"
