f19069ee5ac3bfd929839941ef613dd6b1ecc6ff
hotio <hotio@protonmail.com>
2019-11-16 10:08:49 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq -r .tag_name | sed s/v//g)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG BAZARR_VERSION=.*$/ARG BAZARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 echo "##[set-output name=version;]${version}"
