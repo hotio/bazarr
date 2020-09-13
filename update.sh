8e2c7c597295c95b9749550702248f02dd774d4b
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 15:10:45 +0200

    tweak build

--- update.sh
+++ update.sh
@@ -9,9 +9,26 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Check if app works..."
+    app_url="http://localhost:6767"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL "${app_url}" > /dev/null
+    status=$?
+    echo "Show docker logs..."
+    docker logs service
+    exit ${status}
+elif [[ ${1} == "screenshot" ]]; then
+    app_url="http://localhost:6767"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    docker run --rm --network host --entrypoint="" -u "$(id -u "$USER")" -v "${GITHUB_WORKSPACE}":/usr/src/app/src zenika/alpine-chrome:with-puppeteer node src/puppeteer.js
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/morpheus65535/bazarr/commits/development" | jq -r .sha)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{BAZARR_VERSION=[^}]*}/{BAZARR_VERSION=${version}}/g" .github/workflows/build.yml
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
