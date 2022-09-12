6c5258e057dfeaa18c5874cf642cc67722b96dc3
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-12 12:20:05 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -32,5 +32,6 @@ else
     old_version=$(jq -r '.version' < VERSION.json)
     changelog=$(jq -r '.changelog' < VERSION.json)
     [[ "${old_version}" != "${version}" ]] && changelog="https://github.com/morpheus65535/bazarr/compare/v${old_version}...v${version}"
-    echo '{"version":"'"${version}"'","changelog":"'"${changelog}"'","bbranch":"development"}' | jq . > VERSION.json
+    version_json=$(cat ./VERSION.json)
+    jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'" | .bbranch = "'"development"'"' <<< "${version_json}" > VERSION.json
 fi
