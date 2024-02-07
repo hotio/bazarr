f39af9b4c92cde49c68f365043092edab495a737
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:10:58 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq -re .tag_name)
-[[ -z ${version} ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
