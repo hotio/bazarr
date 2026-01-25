d1e286a25b7897633113e893b337261df0d3926d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-25 04:02:06 +0100

    add auth [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-version=$(curl -fsSL "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq -re .tag_name)
+version=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq -re .tag_name)
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
